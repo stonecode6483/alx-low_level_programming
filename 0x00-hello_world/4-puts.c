510a5ddea9454506e8ddca216b8eb51030c188dc
lateef <lateefadekola90@gmail.com>
2023-08-12 15:01:33 -0700

    puts c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programme that print a line with put  function
+ * Return:0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle );
+	Return (0);
+}
