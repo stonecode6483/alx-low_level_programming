326ac20bc167b634b979ce325668190d8dfa6821
lateef <lateefadekola90@gmail.com>
2023-08-14 22:17:25 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A programme that print a line with put  function
- * Return:0(success)
+  * Return: 0 (success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-Return (0);
+Return(0);
 }
