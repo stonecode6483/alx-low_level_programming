1e3294e7856cc66be98003672f0b329cfbfcbfcf
stonecode6483 <lateefadekola90@gmail.com>
2023-10-05 14:14:25 -0700

    sice.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 /**
- * main - main function
- *
- * Return: 0 on success
- */
+ * main - A programme that print the size of various computer type.
+ * Return 0(success)
+ * */
 int main(void)
 {
-printf("size of char: %lu byte(s)", sizeof(char));
-printf("size of int: %lu byte(s)", sizeof(int));
-printf("size of long int: %lu byte(s)", sizeof(long int));
-printf("size of long long int: %lu byte(s)", sizeof(long long int));
-printf("size of float: %lu byte(s)", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of char: %lu byte(s)/n",(unsigned long)sizeof(a));
+printf("size of int: %lu byte(s)/n",(unsigned long)sizeof(b));
+printf("size of long int: %lu byte(s)/n",(unsigned long)sizeof(c));
+printf("size of long long int: %lu byte(s)/n",(unsigned long)sizeof(d));
+printf("size of float: %lu byte(s)/n",(unsigned long)sizeof(f));
 	return (0);
 }
