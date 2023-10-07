c66d981d6112fe9029fcab27656ada6279dfc78d
stonecode6483 <lateefadekola90@gmail.com>
2023-10-07 13:17:26 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c  <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z')
+		{
+			putchar(d);
+				d++;
+		}
+	putchar('\n');
+	return (0);
+}
