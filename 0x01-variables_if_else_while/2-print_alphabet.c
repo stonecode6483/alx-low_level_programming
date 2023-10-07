6cb0517ccecc6eef1c549984b884e4f6be29eda3
stonecode6483 <lateefadekola90@gmail.com>
2023-10-07 12:33:53 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while (c <= 'z') {
+	       putchar(c);
+c++;
+	}
+	putchar('\n');
+Return:(0);
+}
