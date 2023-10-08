d28dfc32146a5a204316a277583247936f805ebc
stonecode6483 <lateefadekola90@gmail.com>
2023-10-08 10:04:05 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - prog entry point
  * Return: 0 (success)
  */
 int main(void)
 {
 	int a;
-
+	
 	for (a = 0; a < 10; a++);
 	{
 		putchar(a + '0');
