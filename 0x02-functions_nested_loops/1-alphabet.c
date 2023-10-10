2ccac3c07cb36f633884470b1ae30d0135eb23ea
stonecode6483 <lateefadekola90@gmail.com>
2023-10-10 09:05:42 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+ * print_alphabet - function to print alphabet
+ * return: 0
+ */
+
+void print_alphabet(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+		_putchar(t);
+	_putchar('\n');
+}
