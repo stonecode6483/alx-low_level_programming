02890990414f8b396de92cf8c0969543a3210115
stonecode6483 <lateefadekola90@gmail.com>
2023-10-10 09:33:57 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lower case x10
+ * return: 0
+*/
+
+void print_alphabet_x10(void)
+{
+	char t;
+	int u;
+
+	for (u = 0 ; u < 10 ; u++)
+	{
+		for (t = 'a' ; t <= 'z' ; t++)
+			_putchar(t);
+	}
+	_putchar('\n');
+}
