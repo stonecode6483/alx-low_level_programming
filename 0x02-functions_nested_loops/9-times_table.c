11b06bb26a9dc1883a967d01bda6ef32d07300aa
stonecode6483 <lateefadekola90@gmail.com>
2023-10-11 22:02:05 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,12 +15,12 @@ void times_table(void)
 		{
 			result = a * b;
 			if (b == 0)
-				printf("%d,", result);
+				printf("%d, ", result);
 			else
 			{
 				printf("%2d", result);
 				if (b != 9)
-					printf(",");
+					printf(", ");
 			}
 
 		}
