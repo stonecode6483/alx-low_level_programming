591ce3577fb679f517e517fe98a1a6edd2b53145
stonecode6483 <lateefadekola90@gmail.com>
2023-10-17 01:10:13 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts2 - print every character of a string,starting with the first character,
+ * and a new line
+ * @str: string
+ * Return: string
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			putchar(str[o]);
+		}
+	}
+	putchar('\n');
+}
+
