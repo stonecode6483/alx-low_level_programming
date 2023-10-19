8d55d3d58ac3b5722208d003c46f4cad164fb4eb
stonecode6483 <lateefadekola90@gmail.com>
2023-10-19 12:08:00 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-* strcat - concatenates two strings.
-* @dest:input value
-* @src:input value
-* Return:void
+* _strcat - a function that concatenates two strings
+* @dest: input value
+* @src: input value
+*
+* Return: void
 */
 char *_strcat(char *dest, char *src)
 {
-
-int j;
 int i;
-
+int j;
 
 i = 0;
 while (dest[i] != '\0')
@@ -21,9 +20,11 @@ j = 0;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
-j++;
 i++;
+j++;
 }
+
 dest[i] = '\0';
 return (dest);
 }
+
