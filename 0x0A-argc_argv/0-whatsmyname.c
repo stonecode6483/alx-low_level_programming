72a00db61c094e327349114795670dff088c4876
stonecode6483 <lateefadekola90@gmail.com>
2023-10-29 22:49:16 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - print the name of the program
+ * @argv: argument array
+ * @argc:argument array
+ * Return: 0
+ */
+int main(int argc__attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
