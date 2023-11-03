21c688134e21249cb6dd0c917a4876666a553be3
stonecode6483 <lateefadekola90@gmail.com>
2023-11-03 13:04:42 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -4,10 +4,9 @@
 #include <limits.h>
 
 /**
-*  * main - check the code
-*   *
-*    * Return: Always 0.
-*     */
+ * main - check the code
+ * Return: Always 0.
+ */
 int main(void)
 {
 char *c;
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - function that concatenate two string.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s1
