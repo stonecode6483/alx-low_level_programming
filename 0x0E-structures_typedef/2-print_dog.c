88d15dd2814dde487f88b8348559d4a860663c8c
stonecode6483 <lateefadekola90@gmail.com>
2023-11-06 13:45:19 -0800

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		d->name  = "(nil)";
+	
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
