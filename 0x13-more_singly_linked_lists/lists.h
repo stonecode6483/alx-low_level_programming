3a5bcf8dde3e30f0b1afed641330bb17fca22aa0
stonecode6483 <lateefadekola90@gmail.com>
2023-11-20 12:55:45 -0800

    lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -9,7 +10,6 @@
  * Description: singly linked list node structure
  *
  */
-
 typedef struct listint_s
 {
 	int n;
