c0c05d140168f41da417f09cfabb54a41d10badc
stonecode6483 <lateefadekola90@gmail.com>
2023-11-27 23:54:42 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int file_l;
 
-	if (filename)
+	if (!filename)
 		return (-1);
 	file_l = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_l == -1)
