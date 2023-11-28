064fb230527f7496c2155add938b0269a0f01b1c
stonecode6483 <lateefadekola90@gmail.com>
2023-11-27 23:40:58 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - a function that creates a file
  * @filename: name of file to be createded
  * @text_content:content in the file
- * Return; 1 if success,-1 if fail
+ * Return: 1 if success,-1 if fail
  */
 int create_file(const char *filename, char *text_content)
 {
