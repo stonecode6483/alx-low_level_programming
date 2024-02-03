295123b5ec32b07d38bdb237938c029f9ede58a9
stonecode6483 <lateefadekola90@gmail.com>
2024-02-03 11:16:42 -0800

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned long int i;
+hash_node_t *current_node, *next_node;
+
+if (ht == NULL)
+return;
+
+for (i = 0; i < ht->size; i++)
+{
+current_node = ht->array[i];
+while (current_node != NULL)
+{
+next_node = current_node->next;
+free(current_node->key);
+free(current_node->value);
+free(current_node);
+current_node = next_node;
+}
+}
+
+free(ht->array);
+free(ht);
+}
